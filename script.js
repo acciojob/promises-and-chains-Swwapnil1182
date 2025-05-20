8c2fbdf82eed654f10f7bb0b475d5c2ddc3ae49b
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-20 21:19:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+
+function onSubmit(){
+    event.preventDefault();
+
+    let age=document.getElementById("age").value.trim();
+    let name1=document.getElementById("name").value.trim();
+    if(age==="" || name1===""){
+        alert("Please enter valid details.")
+        return false
+    }   
+
+     let myPromise=new Promise((resolve,reject)=>{
+     age = parseInt(age);
+
+    setTimeout(()=>{
+            if(age>18){
+                resolve(alert('Welcome, . You can vote.'));
+            }
+            else{
+                reject(alert("Oh sorry . You aren't old enough."));
+            }
+    },4000)
+})
+
+myPromise
+    .then(message => alert(message))
+    .catch(error => alert(error));
+
+}
+
+
+
